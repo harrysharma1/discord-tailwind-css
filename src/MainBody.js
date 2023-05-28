7f6787e5378561422e48a37b4fdafdafc1b2ef1b
Harry Sharma <harrysharma1066@gmail.com>
2023-05-28 17:44:28 +0100

    Fix posts

--- src/MainBody.js
+++ src/MainBody.js
@@ -5,10 +5,85 @@ function MainBody() {
   return (
     <div className="main-body">
         <NavBar/>
+        <div className="main-body-content">
+        <Post
+          username="Luffy"
+          text="lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."
+          timestamp="12 hours ago"
+        />
+
+        <Post
+          username="Zoro"
+          text="lorem sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          timestamp="8 hours ago"
+        />
+
+
+        <Post
+          username="Sanji"
+          text="
+          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+          timestamp="6 hours ago"
+        />
+
+
+        <Post
+          username="Brook"
+          text="
+          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. piscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem.
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. piscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem.
+          piscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem.
+          "
+          timestamp="5 hours ago"
+        />
+
+        <Post
+          username="Robin"
+          text="
+          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. piscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem.
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. piscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem.    "
+          timestamp="5 hours ago"
+        />
+
         
-      
+        </div>
+   
+        
+    
     </div>
   )
+}
+const Post = ({
+   username, 
+   timestamp, 
+   text 
+  }) => {
+
+  const seed = Math.round(Math.random() * 100);
+  return (
+    <div className="post">
+      <div className='avatar-wrapper'>
+        <img src={`https://avatars.dicebear.com/api/open-peeps/${seed}.svg`} alt='' className='avatar' />
+      </div>
+
+      <div className='post-content'>
+        <p className='post-owner'>
+          {username}
+          <small className="timestamp">{timestamp}</small>
+        </p>
+        <p className='post-text'>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const ChatBar = () => {
+  return(
+    <div className="search-bar">
+      
+
+    </div>
+  ) 
 }
 
 export default MainBody
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import {useState} from 'react'
 import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
 function NavBar() {
+    const [icon,setIcon] = useState(false);
+    const handleIconChange = () => {
+            setIcon(!icon);
+    }
   return (
     <div className="nav-bar">
         <div className='title-text'>
             <FaIcons.FaDiscord size={25}/>
             <Title/>
         </div>
-   
         <SearchBar/>
+
+        <div onClick={handleIconChange}>
+            {icon? <AiIcons.AiFillBell size={25} className="text-gray-500 cursor-pointer"/>:<AiIcons.AiOutlineBell size={25} className="text-gray-500 cursor-pointer"/>}
+        </div>
+        <FaIcons.FaUserCircle size={25} className="text-gray-500 cursor-pointer"/>
+       
     </div>
   )
 }
@@ -16,7 +27,7 @@ const SearchBar = ()=> {
     return(
         <div className='search'>
         <input className='search-input' type='text' placeholder='Search...' />
-        <FaIcons.FaSearch size='18' className='text-secondary my-auto' />
+        <FaIcons.FaSearch size='18' className='text-secondary my-auto cursor-pointer' />
       </div>
     )
 
